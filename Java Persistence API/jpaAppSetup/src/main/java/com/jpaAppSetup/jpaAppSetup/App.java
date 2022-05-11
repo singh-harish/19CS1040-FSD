e2b8c27f61b9c5a7daa0d1678c45896664b414aa
Harish Singh <singhharish734@gmail.com>
2022-05-11 23:55:18 +0530

    jpaSetupApp Update with CRUD

--- Java Persistence API/jpaAppSetup/src/main/java/com/jpaAppSetup/jpaAppSetup/App.java
+++ Java Persistence API/jpaAppSetup/src/main/java/com/jpaAppSetup/jpaAppSetup/App.java
@@ -10,21 +10,50 @@ public class App
     {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
         EntityManager em = emf.createEntityManager();
+//      ------------------------------------
+//      FIND AN ENTITY
+//      ------------------------------------
         
 //        Student stud = em.find(Student.class, 2);
 //        System.out.println(stud);
+
+//      ------------------------------------
+//      PERSIST AN ENTITY
+//      ------------------------------------   
         
+//        em.getTransaction().begin();
+//        
+//        Student s1= new Student();
+//        s1.setId(4);
+//        s1.setName("Shyam");
+//        s1.setDept("CE");
+//        
+//        em.persist(s1);
+//        em.getTransaction().commit();
+//        
+//        em.close();
+//        emf.close();
+
+//      ------------------------------------
+//      UPDATE AN ENTITY
+//      ------------------------------------
+       
+//        Student stud = em.find(Student.class, 2);
+//        
+//        System.out.println(stud);
+//        stud.setDept("CIVIL");
+//        System.out.println(stud);
+        
+//      ------------------------------------
+//      DELETE AN ENTITY
+//      ------------------------------------
+       
         em.getTransaction().begin();
-        
-        Student s1= new Student();
-        s1.setId(4);
-        s1.setName("Shyam");
-        s1.setDept("CE");
-        
-        em.persist(s1);
+        Student stud = em.find(Student.class, 2);
+        em.remove(stud);
         em.getTransaction().commit();
         
         em.close();
-        emf.close();
+        emf.close();;
     }
 }
